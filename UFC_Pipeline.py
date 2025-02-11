04c4b0077d2eb1d91746586556b826165d35080d
Mocodafrog <maupintoflores@gmail.com>
2025-02-11 09:03:38 -0500

    Update UFC_Pipeline.py

--- UFC_Pipeline.py
+++ UFC_Pipeline.py
@@ -793,120 +793,6 @@ for col in numeric_columns:
 #fight_stats_winsorized[numeric_columns] = scaler.fit_transform(fight_stats_winsorized[numeric_columns])
 
 
-# Definir las características (X) y la variable objetivo (y)
-X = fight_stats.drop(columns=['Winner', 'Method', 'Event', 'Fight', 'Fighter'])
-y_winner = fight_stats['Winner']  
-y_method = fight_stats['Method']
-
-# Dividir los datos en conjuntos de entrenamiento y prueba
-X_train, X_test, y_train_winner, y_test_winner = train_test_split(X, y_winner, test_size=0.2, random_state=42)
-_, _, y_train_method, y_test_method = train_test_split(X, y_method, test_size=0.2, random_state=42)
-
-# Definir los modelos y sus parámetros para GridSearch
-models = {
-    'XGBoost': (XGBClassifier(), {
-        'learning_rate': [0.01, 0.1],
-        'n_estimators': [100, 200],
-        'max_depth': [3, 5, 7]
-    }),
-    'LightGBM': (LGBMClassifier(), {
-        'learning_rate': [0.01, 0.1],
-        'n_estimators': [100, 200],
-        'max_depth': [3, 5, 7]
-    }),
-    'CatBoost': (CatBoostClassifier(verbose=0), {
-        'learning_rate': [0.01, 0.1],
-        'depth': [3, 5, 7],
-        'iterations': [100, 200]
-    }),
-    'RandomForest': (RandomForestClassifier(), {
-        'n_estimators': [100, 200],
-        'max_depth': [10, 20],
-        'min_samples_split': [2, 5]
-    }),
-    'GradientBoosting': (GradientBoostingClassifier(), {
-        'learning_rate': [0.01, 0.1],
-        'n_estimators': [100, 200],
-        'max_depth': [3, 5, 7]
-    }),
-    'MLPClassifier': (MLPClassifier(), {
-        'hidden_layer_sizes': [(100,), (150,)],
-        'learning_rate_init': [0.001, 0.01],
-        'max_iter': [200, 300],
-        'activation': ['relu', 'tanh']
-    }),
-    'LogisticRegression': (LogisticRegression(max_iter=1000), {
-        'C': [0.1, 1, 10],
-        'solver': ['lbfgs', 'liblinear']
-    }),
-    'KNeighbors': (KNeighborsClassifier(), {
-        'n_neighbors': [3, 5, 7],
-        'weights': ['uniform', 'distance']
-    }),
-    'SVC': (SVC(probability=True), {
-        'C': [0.1, 1, 10],
-        'kernel': ['linear', 'rbf']
-    }),
-    'ExtraTrees': (ExtraTreesClassifier(), {
-        'n_estimators': [100, 200],
-        'max_depth': [10, 20],
-        'min_samples_split': [2, 5]
-    })
-}
-
-
-base_models_winner = [
-    ('XGBoost', XGBClassifier(learning_rate=0.1, max_depth=5, n_estimators=100)),
-    ('LightGBM', LGBMClassifier(learning_rate=0.1, max_depth=3, n_estimators=200)),
-    ('CatBoost', CatBoostClassifier(depth=5, iterations=200, learning_rate=0.1)),
-    ('RandomForest', RandomForestClassifier(max_depth=20, min_samples_split=2, n_estimators=100)),
-    ('GradientBoosting', GradientBoostingClassifier(learning_rate=0.1, max_depth=5, n_estimators=200)),
-    ('LogisticRegression', LogisticRegression(C=10, solver='liblinear',max_iter=500 )),  # C=10 confirmado
-    ('ExtraTrees', ExtraTreesClassifier(max_depth=20, min_samples_split=2, n_estimators=200)),
-    ('SVC', SVC(C=1, kernel='linear', probability=True))
-]
-
-# Metaclassifier para predicción del ganador
-final_model_winner = LogisticRegression()
-
-# Stacking ensemble para GANADOR
-stacking_winner = StackingClassifier(estimators=base_models_winner, final_estimator=final_model_winner, cv=3)
-
-# Entrenar el modelo para predicción del ganador
-stacking_winner.fit(X_train, y_train_winner)
-
-# Predicción y evaluación para GANADOR
-y_pred_winner = stacking_winner.predict(X_test)
-y_pred_proba_winner = stacking_winner.predict_proba(X_test)
-accuracy_winner = accuracy_score(y_test_winner, y_pred_winner)
-
-
-
-# **Modelos y parámetros para predicción del MÉTODO**
-base_models_method = [
-    ('XGBoost', XGBClassifier(learning_rate=0.1, max_depth=7, n_estimators=100)),
-    ('LightGBM', LGBMClassifier(learning_rate=0.1, max_depth=5, n_estimators=100)),
-    ('CatBoost', CatBoostClassifier(depth=3, iterations=200, learning_rate=0.1)),
-    ('RandomForest', RandomForestClassifier(max_depth=20, min_samples_split=2, n_estimators=200)),
-    ('GradientBoosting', GradientBoostingClassifier(learning_rate=0.1, max_depth=5, n_estimators=100)),
-    ('LogisticRegression', LogisticRegression(C=10, solver='liblinear',max_iter=500 )),
-    ('SVC', SVC(C=10, kernel='linear', probability=True)),
-    ('ExtraTrees', ExtraTreesClassifier(max_depth=20, min_samples_split=5, n_estimators=200))
-]
-
-# Metaclassifier para predicción del método
-final_model_method = LogisticRegression()
-
-# Stacking ensemble para MÉTODO
-stacking_method = StackingClassifier(estimators=base_models_method, final_estimator=final_model_method, cv=3)
-
-# Entrenar el modelo para predicción del método
-stacking_method.fit(X_train, y_train_method)
-
-# Predicción y evaluación para MÉTODO
-y_pred_method = stacking_method.predict(X_test)
-y_pred_proba_method = stacking_method.predict_proba(X_test)
-accuracy_method = accuracy_score(y_test_method, y_pred_method)
 
 
 def calcular_ultimos_5_combates(df):
@@ -942,7 +828,6 @@ print(df_estadisticas_ultimos_5)
 
 # Definir rutas absolutas
 DATA_DIR = os.path.abspath('data')
-MODELS_DIR = os.path.abspath('models')
 
 # Crear los directorios si no existen
 os.makedirs(DATA_DIR, exist_ok=True)
@@ -957,21 +842,6 @@ try:
 except Exception as e:
     print(f"Error al guardar columnas_X: {e}")
 
-# Guardar el modelo de ganador en 'models/stacking_winner.pkl'
-stacking_winner_path = os.path.join(MODELS_DIR, 'stacking_winner.pkl')
-try:
-    joblib.dump(stacking_winner, stacking_winner_path)
-    print(f"Modelo guardado en: {stacking_winner_path}")
-except Exception as e:
-    print(f"Error al guardar stacking_winner: {e}")
-
-# Guardar el modelo de método de pelea en 'models/stacking_method.pkl'
-stacking_method_path = os.path.join(MODELS_DIR, 'stacking_method.pkl')
-try:
-    joblib.dump(stacking_method, stacking_method_path)
-    print(f"Modelo guardado en: {stacking_method_path}")
-except Exception as e:
-    print(f"Error al guardar stacking_method: {e}")
 
 # Guardar las estadísticas de los últimos 5 combates en 'data/df_estadisticas_ultimos_5.csv'
 estadisticas_path = os.path.join(DATA_DIR, 'df_estadisticas_ultimos_5.csv')
