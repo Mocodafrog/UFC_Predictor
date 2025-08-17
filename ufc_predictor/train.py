ac263d8ea33086d18bbe102593f9721fcead925c
Mocodafrog <maupintoflores@gmail.com>
2025-08-16 20:40:24 -0500

    Define class min and target suffix after encoding y

--- ufc_predictor/train.py
+++ ufc_predictor/train.py
@@ -122,6 +122,10 @@ def train(
         columnas_procesadas = [c for c in columnas_procesadas if c not in no_numericas]
 
     y = fight_stats[target_column]
+    encoder = LabelEncoder()
+    y = pd.Series(encoder.fit_transform(y), name=target_column)
+    min_clase = y.value_counts().min()
+    target_suffix = target_column.lower()
 
 
     if models is None:
